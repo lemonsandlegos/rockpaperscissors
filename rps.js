4ea7d8ab42352f4a35da5ef35c39094c693b0517
Jordan <jordanbeth017@gmail.com>
2021-06-23 09:35:22 -0400

    added variable to track wins

--- rps.js
+++ rps.js
@@ -1,29 +1,40 @@
 let rockPaperScissors = ["rock", "paper", "scissors"];
-let computerselection = computerPlay();
-let playerselection = playerPlay();
-console.log(computerPlay());
+let wins = 0;
+let losses = 0;
+
+game();
+if (wins > losses) {alert("Congratulations! You win the game!")}
+else if (losses > wins) {alert("Sorry! You lost the game.")}
 
 function computerPlay()
 {return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];}
 
 function playerPlay()
-{prompt("Rock…Paper…Scissors…Shoot!").toLowerCase();}
+{return prompt("Rock…Paper…Scissors…Shoot!").toLowerCase();}
 
 function playRound(playerselection, computerselection){
-    if (playerselection === "rock" && computerselection === "rock") {return "it's a tie!"}
+    if (playerselection === "rock" && computerselection === "rock") {return "it's a tie!";}
     else if (playerselection === "paper" && computerselection === "paper") {return "it's a tie!"}
     else if (playerselection === "scissors" && computerselection === "scissors") {return "it's a tie!"}
-    else if (playerselection === "paper" && computerselection === "rock") {return "You win! Paper covers rock!"}
-    else if (playerselection === "rock" && computerselection === "paper") {return "You lose! Paper covers rock!"}
-    else if (playerselection === "paper" && computerselection === "scissors") {return "You lost! Scissors cuts paper!"}
-    else if (playerselection === "scissors" && computerselection === "paper") {return "You won! Scissors cuts paper!"}
-    else if (playerselection === "scissors" && computerselection === "rock") {return "You lost! Rock breaks scissors!"}
-    else if (playerselection === "rock" && computerselection === "scissors") {return "You won! Rock breaks scissors!"}    
+    else if (playerselection === "paper" && computerselection === "rock") {return "You win! Paper covers rock!";
+    wins = wins + 1;}
+    else if (playerselection === "rock" && computerselection === "paper") {return "You lose! Paper covers rock!";
+    losses = losses + 1;}
+    else if (playerselection === "paper" && computerselection === "scissors") {return "You lost! Scissors cuts paper!";
+    losses = losses + 1;}
+    else if (playerselection === "scissors" && computerselection === "paper") {return "You won! Scissors cuts paper!";
+    wins = wins + 1;}
+    else if (playerselection === "scissors" && computerselection === "rock") {return "You lost! Rock breaks scissors!";
+    losses = losses + 1;}
+    else if (playerselection === "rock" && computerselection === "scissors") {return "You won! Rock breaks scissors!";
+    wins = wins + 1;}    
 }
-console.log(playRound(playerselection, computerselection));
 
 function game(){
 for (let i = 0; i<5; i++) {
-playRound(playerselection, computerselection);
+    let computerselection = computerPlay();
+    let playerselection = playerPlay();
+    console.log(computerselection);
+    console.log(playerselection);
 console.log(playRound(playerselection, computerselection));
 }}
