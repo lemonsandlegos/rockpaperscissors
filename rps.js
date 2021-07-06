60886bf3a2672f19941e32169d586fb2fa3e6e03
Jordan <jordanbeth017@gmail.com>
2021-07-06 13:55:12 -0400

    Updated Javascript and HTML to extend functionality

--- rps.js
+++ rps.js
@@ -1,45 +1,109 @@
 let rockPaperScissors = ["rock", "paper", "scissors"];
-let wins = 0;
-let losses = 0;
 
 game();
-if (wins > losses) {alert("Congratulations! You win the game!")}
-else if (losses > wins) {alert("Sorry! You lost the game.")}
-else {alert("The game ended in a tie!")}
-
-function computerPlay()
-{return rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];}
-
-function playerPlay()
-{return prompt("Rock…Paper…Scissors…Shoot!").toLowerCase();}
-
-function playRound(playerselection, computerselection){
-    if (playerselection === computerselection) {return "it's a tie!";}
-    else if (playerselection === "paper" && computerselection === "rock") {
-        wins = wins + 1;
-        return "You win! Paper covers rock!";}
-    else if (playerselection === "rock" && computerselection === "paper") {
-        losses = losses + 1;
-        return "You lose! Paper covers rock!";}
-    else if (playerselection === "paper" && computerselection === "scissors") {
-        losses = losses + 1;
-        return "You lost! Scissors cuts paper!";}
-    else if (playerselection === "scissors" && computerselection === "paper") {
-        wins = wins + 1;
-        return "You won! Scissors cuts paper!";}
-    else if (playerselection === "scissors" && computerselection === "rock") {
-        losses = losses + 1;
-        return "You lost! Rock breaks scissors!";}
-    else if (playerselection === "rock" && computerselection === "scissors") {
-        wins = wins + 1;
-        return "You won! Rock breaks scissors!";}    
-}
 
 function game(){
-for (let i = 0; i<5; i++) {
-    let computerselection = computerPlay();
-    let playerselection = playerPlay();
-    console.log(computerselection);
-    console.log(playerselection);
-console.log(playRound(playerselection, computerselection));
-}}
+    playerPoints = 0;
+    computerPoints = 0;
+    document.getElementById("rock").addEventListener("click", functionRock);
+    document.getElementById("paper").addEventListener("click", functionPaper);
+    document.getElementById("scissors").addEventListener("click", functionScissors);
+}
+
+function functionRock() {
+    playerselection = "rock";
+    computerselection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+    playRound(playerselection, computerselection);
+    if (playerPoints < 5 && computerPoints > 4) {
+        document.getElementById("finalresults").innerHTML = "Sorry! You lost the game. Better luck next time :)"
+    }
+    if (playerPoints > 4 && computerPoints < 5) {
+        document.getElementById("finalresults").innerHTML = "Congratulations! You won the game! :)"
+    }
+    if (playerPoints === 5 && computerPoints === 5) {
+        document.getElementById("finalresults").innerHTML = "You tied the game. Good job...kinda."
+    }
+}
+
+function functionPaper() {
+    playerselection = "paper";
+    computerselection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+    playRound(playerselection, computerselection);
+    if (playerPoints < 5 && computerPoints > 4) {
+        document.getElementById("finalresults").innerHTML = "Sorry! You lost the game. Better luck next time :)"
+    }
+    if (playerPoints > 4 && computerPoints < 5) {
+        document.getElementById("finalresults").innerHTML = "Congratulations! You won the game! :)"
+    }
+    if (playerPoints === 5 && computerPoints === 4) {
+        document.getElementById("finalresults").innerHTML = "You tied the game. Good job...kinda."
+    }
+}
+
+function functionScissors() {
+    playerselection = "scissors";
+    computerselection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+    playRound(playerselection, computerselection);
+    if (playerPoints < 5 && computerPoints > 4) {
+        document.getElementById("finalresults").innerHTML = "Sorry! You lost the game. Better luck next time :)"
+    }
+    if (playerPoints > 4 && computerPoints < 5) {
+        document.getElementById("finalresults").innerHTML = "Congratulations! You won the game! :)"
+    }
+    if (playerPoints === 5 && computerPoints === 4) {
+        document.getElementById("finalresults").innerHTML = "You tied the game. Good job...kinda."
+    }
+}
+
+
+
+function playRound(playerselection, computerselection) {
+    if (playerselection === computerselection) {
+        document.getElementById("results").innerHTML = "It's a tie!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "it's a tie!";
+    }
+    else if (playerselection === "paper" && computerselection === "rock") {
+        playerPoints = playerPoints + 1;
+        document.getElementById("results").innerHTML = "You win! Paper covers rock!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You win! Paper covers rock!";
+    }
+    else if (playerselection === "rock" && computerselection === "paper") {
+        computerPoints = computerPoints + 1;
+        document.getElementById("results").innerHTML = "You lose! Paper covers rock!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You lose! Paper covers rock!";
+    }
+    else if (playerselection === "paper" && computerselection === "scissors") {
+        computerPoints = computerPoints + 1;
+        document.getElementById("results").innerHTML = "You lost! Scissors cut paper!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You lost! Scissors cuts paper!";
+    }
+    else if (playerselection === "scissors" && computerselection === "paper") {
+        playerPoints = playerPoints + 1;
+        document.getElementById("results").innerHTML = "You win! Scissors cut paper!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You won! Scissors cuts paper!";
+    }
+    else if (playerselection === "scissors" && computerselection === "rock") {
+        computerPoints = computerPoints + 1;
+        document.getElementById("results").innerHTML = "You lost! Rock breaks scissors!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You lost! Rock breaks scissors!";
+    }
+    else if (playerselection === "rock" && computerselection === "scissors") {
+        playerPoints = playerPoints + 1;
+        document.getElementById("results").innerHTML = "You won! Rock breaks scissors!";
+        document.getElementById("playerScore").innerHTML = "You: " + playerPoints;
+        document.getElementById("computerScore").innerHTML = "Opponent: " + computerPoints;
+        return "You won! Rock breaks scissors!";
+    }
+}
